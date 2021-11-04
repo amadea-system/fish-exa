bf10a140fd63663a27e99483da4e15fb91cc1071
Evan Pitstick <epitstick@paloaltonetworks.com>
2021-11-04 17:50:34 -0400

    mistakes were made

--- conf.d/fish-exa.fish
+++ conf.d/fish-exa.fish
@@ -1,6 +1,6 @@
 function __fish_exa_install --on-event fish-exa_install
     set -U EXA_STANDARD_OPTIONS "--git" "--header" "--icons"
-    set -U EXA_LL_OPTIONS "--long" "--group" "--extended"
+    set -U EXA_LL_OPTIONS "--long" "--group"
     set -U EXA_LA_OPTIONS "--all" "--binary" "--links" "--inode" "--blocks"
     set -U EXA_LD_OPTIONS "--list-dirs"
     set -U EXA_LE_OPTIONS "--extended" "--created" "--modified" "--accessed" "--changed"
@@ -22,7 +22,6 @@ function __fish_exa_uninstall --on-event fish-exa_uninstall
     functions --erase l
     # We don't erase ls here because it could be the system ls and it doesn't need any options
     functions --erase ll
-    functions --erase exa_git
     functions --erase la
     functions --erase ld
     functions --erase lt
--- functions/lt.fish
+++ functions/lt.fish
@@ -1,3 +1,3 @@
-function lt --description 'exa with $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS'
+function lt --description 'exa with $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS (by default requires an int as first arg)'
   exa $EXA_STANDARD_OPTIONS $EXA_LT_OPTIONS $argv; 
 end
