2ccc7ece369b428914a6407edc94a721e069f789
Amadea <53259552+amadea-system@users.noreply.github.com>
2022-03-05 15:06:55 -0500

    Update fish-exa.fish

--- conf.d/fish-exa.fish
+++ conf.d/fish-exa.fish
@@ -1,9 +1,9 @@
 function __fish_exa_install --on-event fish-exa_install
-    set -U EXA_STANDARD_OPTIONS "--git" "--header" "--icons"
-    set -U EXA_LL_OPTIONS "--long" "--group"
-    set -U EXA_LA_OPTIONS "--all" "--binary" "--links" "--inode" "--blocks"
-    set -U EXA_LD_OPTIONS "--list-dirs"
-    set -U EXA_LE_OPTIONS "--extended" "--created" "--modified" "--accessed" "--changed"
+    set -U EXA_STANDARD_OPTIONS "--git" "--header"              # "--icons"
+    set -U EXA_LL_OPTIONS "--long" "--icons"                    # "--group"
+    set -U EXA_LA_OPTIONS "--all" "--long" "--icons"            # "--binary" "--links" "--inode" "--blocks"
+    set -U EXA_LD_OPTIONS "--only-dirs"
+    set -U EXA_LE_OPTIONS "--extended" "--created" "--modified" "--accessed" "--changed" "--group"
     set -U EXA_LT_OPTIONS "--tree" "--level"
 end
 
@@ -20,11 +20,14 @@ function __fish_exa_uninstall --on-event fish-exa_uninstall
     set --erase EXA_LE_OPTIONS
     set --erase EXA_LT_OPTIONS
     functions --erase l
-    # We don't erase ls here because it could be the system ls and it doesn't need any options
-    functions --erase ll
     functions --erase la
+    functions --erase lad
     functions --erase ld
-    functions --erase lt
-    functions --erase lg
     functions --erase le
+    functions --erase ll
+    functions --erase lld
+    functions --erase llt
+    # We don't erase ls here because it could be the system ls and it doesn't need any options
+    functions --erase lt
+    
 end
